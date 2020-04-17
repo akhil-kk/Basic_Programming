45be0efae15485859f7d5402db3d5499937d3799
akhil-kk <akhilkkopparambil@gmail.com>
2020-04-17 20:41:30 +0530

    program to find the sum of digits of a number

---
+++ 031_sum_of_digits_of_number.js.js
@@ -0,0 +1,13 @@
+// write a program to Sum of digits of a number
+
+const sumOfDigits = (number) => {
+    let sum = 0;
+    let numberToString = String(number);
+    for (let i = 0; i < numberToString.length; i++) {
+        sum = sum + parseInt(numberToString[i]);
+    }
+    return sum;
+}
+
+console.log(sumOfDigits(5856));
+
