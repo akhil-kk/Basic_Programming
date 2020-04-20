701dca2daab85cfb8bc5b6f82bbcee8004be7a41
AKHIL K K <63349004+akhil-kk@users.noreply.github.com>
2020-04-20 18:27:20 +0530

    program to find Greatest of two numbers (#23)

---
+++ 028_greatest_of_two_numbers.js
@@ -0,0 +1,7 @@
+// Write a program to find Greatest of two numbers
+
+const checkWhichIsGreater = (number1, number2) => {
+    return (number1 > number2 ? number1 : number2);
+}
+
+console.log(checkWhichIsGreater(800, 60));
