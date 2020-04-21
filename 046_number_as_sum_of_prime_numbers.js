c15c6975990a136ca1e157fe7500baad350a7310
akhil-kk <akhilkkopparambil@gmail.com>
2020-04-21 17:59:34 +0530

    program to check if a number can be expressed as a sum of two prime numbers

---
+++ 046_number_as_sum_of_prime_numbers.js
@@ -0,0 +1,28 @@
+// Write a program to check if a number be expressed as a sum of two prime numbers?
+
+const checkPrimeNumber = (number) => {
+  let primeNumber = true;
+  for (let i = 2; i < number; i++) {
+    if ((number % i) === 0) {
+      primeNumber = false;
+    }
+  }
+  return primeNumber;
+}
+
+const numberAsSumOfPrimeNumbers = (number1) => {
+  let result;
+  for (let j = 1; j <= number1; j++) {
+    for (let k = 1; k <= number1; k++) {
+      if (checkPrimeNumber(j) === true && checkPrimeNumber(k) === true) {
+        result = j + k;
+        if (result === number1) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+console.log(numberAsSumOfPrimeNumbers(6));
