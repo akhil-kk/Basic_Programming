b847d508c999b92fa2954325a52c373cbf5b4ea3
akhil-kk <akhilkkopparambil@gmail.com>
2020-04-22 22:29:26 +0530

    Program to perform decimal to octal conversion

---
+++ 050_decimal_to_octal.js
@@ -0,0 +1,8 @@
+// Decimal to octal conversion
+
+const decimalToOctal = (decimalNumber) => {
+  let Octalresult = (decimalNumber).toString(8);
+  return Octalresult;
+}
+
+console.log(decimalToOctal(9));
