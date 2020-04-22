21e14376b997ad33cd0af90678feb2859ece26cc
AKHIL K K <63349004+akhil-kk@users.noreply.github.com>
2020-04-22 23:15:02 +0530

    cleaned up branch

--- 050_decimal_to_octal.js
+++
@@ -1,8 +0,0 @@
-// Decimal to octal conversion
-
-const decimalToOctal = (decimalNumber) => {
-  let Octalresult = (decimalNumber).toString(8);
-  return Octalresult;
-}
-
-console.log(decimalToOctal(9));
