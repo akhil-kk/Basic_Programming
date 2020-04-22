0e5428b8ec1e1f43e8b571b82ec7a35239443964
akhil-kk <akhilkkopparambil@gmail.com>
2020-04-22 22:25:00 +0530

    Program to perform decimal to binary conversion

---
+++ 049_decimal_to_binary.js
@@ -0,0 +1,8 @@
+// Decimal to binary conversion
+
+const decimalToBinary = (decimalNumber) => {
+  let binaryResult = (decimalNumber).toString(2);
+  return binaryResult;
+}
+
+console.log(decimalToBinary(15));
