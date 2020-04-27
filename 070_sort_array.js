0c70c60e39d4a2426c75c3136ac36663c8472e7b
akhil-kk <akhilkkopparambil@gmail.com>
2020-04-27 15:45:54 +0530

    Program to sort a string in alphabetical order

---
+++ 070_sort_array.js
@@ -0,0 +1,10 @@
+// Sorting a string in alphabetical order
+
+const sortArray = (sampleArray) => {
+  let sortedArray = [];
+  sortedArray = sampleArray.sort();
+  return sortedArray;
+}
+
+let shuffledArray = ['kilo', 'alpha', 'beta', 'lima', 'romeo']
+console.log(sortArray(shuffledArray))
